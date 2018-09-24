8dec107840fb59aea1814bfc8e4d844befb80d6e
Levi C Uzodike <leviuzodike@gmail.com>
2018-09-24 14:32:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
 }
 
-function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, {[key]: value})
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]= value
+  return object
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]= value
+  return object
 }
