013290af8ab2d3bfaac1085c3ef51a53be4e27f7
Levi C Uzodike <leviuzodike@gmail.com>
2018-09-24 14:30:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 recipes={}
+
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, )
+  return Object.assign({}, object, {[key]: value})
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value})
 }
